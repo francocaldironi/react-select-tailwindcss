377bf5668fc7283e84b643ffd45606a8942da57f
francocaldironi <caldironifranco@gmail.com>
2024-08-27 09:00:08 -0300

    update style

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../src/index.css";
+
+const App = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
+const packageJson = require("./package.json");
+const options = require("./tsconfig.json");
+
+module.exports = {
+    input: "src/index.tsx",
+    output: [
+        {
+            file: packageJson.main,
+            format: "cjs",
+            exports: "auto",
+            sourcemap: true
+        },
+        {
+            file: packageJson.module,
+            format: "esm",
+            exports: "auto",
+            sourcemap: true
+        }
+    ],
+    external: ["react"],
+    plugins: [resolve(), commonjs(), typescript({ ...options.compilerOptions, jsx: "react" })]
+};
